94685613cfddbee65aa5ed24bb9223c195e08f26
abdelrahman mostafa <a.mostafa@iprredsea.local>
2025-05-21 12:59:07 +0300

    asdf

--- startup.sh
+++ startup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Exit on any error
+set -e
+
+# Upgrade pip safely
 pip install --upgrade pip
-pip install -r /home/site/wwwroot/requirements.txt
+
+# Install requirements (you can use relative path here if you're in /home/site/wwwroot)
+pip install -r requirements.txt
+
+# Run the Streamlit app
 python -m streamlit run IPR_APP.py --server.port 8000 --server.enableCORS false
